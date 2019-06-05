8d9fb06f3bd87ceb6e0370559f08d0d0c26e0420
janumedia@yahoo.com.sg <janumedia@gmail.com>
2019-06-05 18:38:58 +0800

    Inital Commit

---
+++ src/components/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let value;
+</script>
+<style type="text/scss">
+div {
+    box-sizing: border-box;
+}
+button {
+    font-size: inherit;
+    //color: blue;
+    border-radius: 15px;
+    padding: 5px 10px;
+}
+</style>
+<button><slot/></button>
